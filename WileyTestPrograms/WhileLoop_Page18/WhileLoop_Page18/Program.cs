69e295753f053e538ac753596b8326719dd78f57
om1979 <blink.oma@gmail.com>
2015-03-03 20:46:58 -0600

    Agregamos Carpete para ARchivos Wiley

---
+++ WileyTestPrograms/WhileLoop_Page18/WhileLoop_Page18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop_Page18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int i = 1;
+            //while (i <= 5)
+            //{
+            //Console.WriteLine("The value of i = {0}", i);
+            //i++;
+
+            //}
+            //Console.ReadLine();
+            int i = 1;
+                do
+                {
+                Console.WriteLine("the value of i = {0}",i);
+                i++;
+                }
+                while (i<=5);
+
+        }
+    }
+}
